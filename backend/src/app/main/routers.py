4d681e17fee7824133ecc23399dbbe29a97c6278
Peper20 <ivan.baluk@bk.ru>
2023-11-16 11:49:35 +0200

    add index.html

--- backend/src/app/main/routers.py
+++ backend/src/app/main/routers.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 
 from app.auth import router as auth_router
+from app.lending import router as lending_router
 
 
 
 
 def init_routers(app: FastAPI) -> None:
 	app.include_router(auth_router)
+	app.include_router(lending_router)
