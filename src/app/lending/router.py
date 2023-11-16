ba75e01f9ceef9091a028ca1fa8dd040838a270b
Peper20 <ivan.baluk@bk.ru>
2023-11-16 12:38:05 +0200

    add endpoint for auth for user

--- src/app/lending/router.py
+++ src/app/lending/router.py
@@ -13,4 +13,10 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get('/', response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse('index.html', {'request': request})
+
+
+@router.get('/login-register', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse('auth.html', {'request': request})
+
