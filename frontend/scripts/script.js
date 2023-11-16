393e7ccd78c210f0a12f774efc0c236377df68c9
Fireek1 <odvdplvdplv@gmail.com>
2023-11-16 10:53:42 +0200

    frontent verstka

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,24 +1,36 @@
-const login = document.querySelector('button')
+const btn = document.querySelector('button')
+const login = document.querySelector('.input-login')
+const password = document.querySelector('.input-password')
 
-login.onclick = async() => {
-    const mail = document.querySelector('.mail').value
-    const password = document.querySelector('.password').value
-    if (mail && password) {
-        let resp = await fetch('http://localhost:8000/auth/jwt/login', {
+btn.onclick = (event) => {
+    event.preventDefault(); // Предотвращаем стандартное поведение отправки формы
+
+    const apiUrl = 'http://localhost:8000/auth/jwt/login';
+    const requestBody = new URLSearchParams({
+        grant_type: '',
+        username: login.value,
+        password: password.value,
+        scope: '',
+        client_id: '',
+        client_secret: '',
+    });
+
+    fetch(apiUrl, {
+            // mode: 'no-cors',
             method: 'POST',
             headers: {
-                'accept': 'application/json'
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'accept': 'application/json',
             },
-            body: new URLSearchParams({
-                'grant_type': '',
-                'username': mail,
-                'password': password,
-                'scope': '',
-                'client_id': '',
-                'client_secret': ''
-            })
+            body: requestBody,
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Обрабатываем данные ответа здесь
+            console.log(data);
+        })
+        .catch(error => {
+            // Обрабатываем ошибки здесь
+            console.error('Error:', error);
         });
-        console.log(resp)
-    } else console.log('Введите пароль')
-
-}
+};
