d796f4b8330d10c281dd3c4c1a42871fb01917b7
Peper20 <ivan.baluk@bk.ru>
2023-11-17 10:53:33 +0200

    lol

--- src/app/lending/router.py
+++ src/app/lending/router.py
@@ -1,6 +1,15 @@
-from fastapi import Depends, APIRouter, Request
+import cv2
+
+
+from fastapi import Depends, APIRouter, Request, WebSocket, WebSocketDisconnect
+from websockets.exceptions import ConnectionClosedError
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from vidgear.gears import NetGear
+
+
+from app.services.cv_detection import prepare_frame
+
 
 
 
@@ -19,4 +28,32 @@ async def index(request: Request):
 @router.get('/login-register', response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse('auth.html', {'request': request})
+
+@router.get('/video-demo', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse('video_demo.html', {'request': request})
+
+
+camera = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+@router.websocket('/video')
+async def get_stream(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            # client = NetGear(receive_mode=True)
+            # client = NetGear(receive_mode=True)
+            # frame = client.recv()
+            success, frame = camera.read()
+
+            if frame is None:
+                break
+
+            prepare_frame(frame)
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            await websocket.send_bytes(buffer.tobytes())
+
+    except (WebSocketDisconnect, ConnectionClosedError):
+        print("Client disconnected")
 
